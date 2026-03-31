af8163c8b2fc81113b3950678ed90e3ead6f24fc
anabeadias <anabeatriz@ibm.com>
2026-03-31 12:46:39 -0300

    Update order.py

--- models/order.py
+++ models/order.py
@@ -1,1 +1,11 @@
 
+from pydantic import BaseModel
+
+class OrderItem(BaseModel):
+    product_id: int
+    quantity: int
+
+class OrderRequest(BaseModel):
+    user_id: int
+    items: list[OrderItem]
+    payment_method: str
